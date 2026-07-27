6012f31af61910ae5f0ef3cc3d67299dce73e888
Hbelagha <hocinebelagha1@gmail.com>
2026-07-27 17:28:37 +0200

    feat: init is now in the constructor to avoid nil state

--- index.js
+++
@@ -1,7 +0,0 @@
-import State from "./state.js";
-
-export {
-    State
-};
-
-export default State;
---
+++ state.ts
@@ -0,0 +1,50 @@
+type StateValue = Record<string, unknown>;
+type StateListener = (newState: StateValue, previousState: StateValue) => void;
+
+class StateManager<T extends StateValue = StateValue> {
+    private state: T;
+    private listeners: Set<StateListener>;
+
+    constructor(initialState: T = {} as T) {
+        this.state = initialState;
+        this.listeners = new Set<StateListener>();
+    }
+
+    // Returns the current state
+    getState(): T {
+        return this.state;
+    }
+
+    // Updates the state with new values
+    setState(newState: Partial<T>): void {
+        const previousState = this.state;
+
+        this.state = {
+            ...this.state,
+            ...newState
+        } as T;
+
+        this.notify(this.state, previousState);
+    }
+
+    // Subscribes a listener to state changes
+    subscribe(callback: StateListener): () => void {
+        this.listeners.add(callback);
+
+        return () => {
+            this.listeners.delete(callback);
+        };
+    }
+
+    // Notifies all listeners when the state changes
+    notify(newState: T, previousState: T): void {
+        this.listeners.forEach((listener) => {
+            listener(newState, previousState);
+        });
+    }
+}
+
+const State = new StateManager<StateValue>({});
+
+export default State;
+export { StateManager };
